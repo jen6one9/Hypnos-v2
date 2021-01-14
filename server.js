dc3cecd9a0947c5de47c3a6be0e904e96eb0c8d7
rancid <robmgraham13@yahoo.com>
2021-01-14 14:03:24 -0800

    removed unnecessary files, formated project with passport and passport DB, added routes for passport signup and authenticated logins, added stylesheets.

--- server.js
+++ server.js
@@ -1,64 +1,30 @@
-var app = require('./config/app');
-var http = require('http');
-var db = require('./models');
+// Requiring necessary npm packages
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
-// Get port from environment and store in Express.
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
-// Create HTTP server.
-var server = http.createServer(app);
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Listen on provided port, on all network interfaces.
-db.sequelize.sync({ alter: true }).then(function(result) {
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
 });
-
-// Normalize a port into a number, string, or false.
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-    return false;
-}
-
-// Event listener for HTTP server "error" event.
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    var bind = typeof port === 'string' ?
-        'Pipe ' + port :
-        'Port ' + port;
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-// Event listener for HTTP server "listening" event.
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string' ?
-        'pipe ' + addr :
-        'port ' + addr.port;
-    // debug('Listening on ' + bind);
-    console.log('Listening on ' + bind);
-}
--- tests/schema.sql
+++
@@ -1,6 +0,0 @@
--- This is the Testing DataBase for the Project --
--- run this program on MySQL WorkBench --
--- after you do that do npm run start:test --
-DROP DATABASE IF EXISTS hypnos_db;
-CREATE DATABASE hypnos_db;
-USE hypnos_db;
