ffda57874ea01c26757ec8d2d0a9b4b5b7b2ebf8
rancid <robmgraham13@yahoo.com>
2021-01-19 20:22:00 -0800

    added starting points for db storage of profile and survey questions

---
+++ public/js/survey.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Getting references to our form and inputs
+    var TimeToSleep = $("input#email-input");
+    var TimeWokeUp = $("input#password-input");
+    var rested = $("form.login");
+
+})
